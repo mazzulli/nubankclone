5b6029a4f7ff7fcd7fe57765d7e52883e1d40545
Danilo Mazzulli <mazzulli@live.com>
2020-04-09 21:50:39 -0300

    Header started

---
+++ src/pages/Main/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container, Title } from './styles';
+import Header from '../../components/Header';
+
+export default function Main(){
+  return (
+    <Container>
+      <Header></Header>
+    </Container>
+  );
+}
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background: #8B10AE;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 13px;
+  color: #FFF;
+`;
